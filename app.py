160b3eb3cf107213ffb9d699dfd444a724d5a262
LRobertoEC <estradacastrejon.luisroberto@utacapulco.edu.mx>
2025-08-06 20:34:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -152,5 +152,4 @@ app = create_app()
 
 # Configuración para ejecución local
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
